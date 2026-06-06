19ad5538835c6474b3ce3c0908a2bf9414b91e62
mirtaksyak02 <40716196+mirtaksyak02@users.noreply.github.com>
2026-06-07 04:15:19 +0500

    Update mp-script.js

--- mp/mp-script.js
+++ mp/mp-script.js
@@ -402,7 +402,7 @@ audioPlayer.addEventListener('timeupdate', () => {
         const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
         progressBar.value = progress;
         currentTimeText.textContent = formatTime(audioPlayer.currentTime);
-        progressBar.style.background = `linear-gradient(to right, #1db954 ${progress}%, #404040 ${progress}%)`;
+        progressBar.style.background = `linear-gradient(to right, #1db954 0%, #ffcc00 ${progress}%, #404040 ${progress}%, #404040 100%)`;
     }
 });
 
